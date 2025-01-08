a26741d93bf39b45ec9c3e2e24698c51147dcd78
alvaro-max-git <alvaro.munoz.pavon@alumnos.upm.es>
2025-01-08 15:27:17 +0100

    Añadido script Python tarea 2, y editado Dockerfile para que lo ejecute (clona el repositorio y edita los archivos)

---
+++ Tarea2/tarea2.py
@@ -0,0 +1,16 @@
+import os
+
+from productpage_app_setup import clonar_repositorio, edicion_archivos
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+PRODUCTPAGE_DIR = os.path.join(BASE_DIR, "practica_creativa2/bookinfo/src/productpage")
+REPO_URL = "https://github.com/CDPS-ETSIT/practica_creativa2"
+
+if __name__ == "__main__":
+    try:
+        clonar_repositorio(repo_url=REPO_URL, base_dir=BASE_DIR)
+        edicion_archivos(productpage_dir=PRODUCTPAGE_DIR)
+
+    except Exception as e:
+        print(f"Error al arrancar la app: {e}")
+        exit(1)
--- pc21-config-mv.py
+++
@@ -1,4 +0,0 @@
-
-
-
-#  sudo virt-copy-in -a PC2.qcow2 holamundo.py /home/cdps
